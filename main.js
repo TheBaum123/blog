c322acfd8a8d4be4e2d3f8cbc7e8bd892a6f9cd1
TheBaum123 <102868970+TheBaum123@users.noreply.github.com>
2023-06-09 00:34:29 +0200

    full rewrite

--- main.js
+++ main.js
@@ -1,34 +1,62 @@
 const express = require("express")
 const parseMd = require("./parsePosts")
 const fs = require("fs")
+const path = require("path")
 
 require("dotenv").config()
 
 const parsedDir = process.env.PARSEDDIR
 
+parseMd()
+
+let postsListed = []
+let cleanListedPosts = []
 
 const app = express()
-
-parseMd()
 
 function reloadPosts() {
     fs.readdir(parsedDir, (err, files) => {
         postsListed = files
+        cleanListedPosts = []
+        files.forEach(file => {
+            let cleanName = path.parse(file).name
+            cleanListedPosts.push({
+                "cleanName": cleanName,
+                "fullName": cleanName
+            })
+        })
+        cleanListedPosts.sort((a, b) => {
+            return(b.cleanName.split("-----")[1] - a.cleanName.split("-----")[1])
+        })
+        let temp = []
+        cleanListedPosts.forEach(postObj => {
+            let newCleanName = postObj.cleanName.split("-----")[0]
+            temp.push({
+                "cleanName": newCleanName,
+                "fullName": postObj.fullName
+            })
+        })
+        cleanListedPosts = temp
     })
 }
 
-reloadPosts()
-
-app.get("*", (req, res) => {
-    if(postsListed.includes(req.query.post)) {
-        reloadPosts()
-        fs.readFile(`${parsedDir}/${req.query.post}`, "utf8", (err, data) => {
-            res.send(data)
+app.get("/", (req, res) => {
+    reloadPosts()
+    if(postsListed.includes(`${req.query.post}.html`)) {
+        fs.readFile(`${parsedDir}/${req.query.post}.html`, "utf8", (err, data) => {
+            if(err) {
+                res.send(`<h1 style="font-family: "monospace"">error</h1>`)
+            } else {
+                res.send(data)
+            }
         })
     } else {
-        reloadPosts()
-        res.json(postsListed).send
+        res.json(cleanListedPosts).send
     }
 })
 
 app.listen(3000)
+
+setTimeout(() => {
+    reloadPosts()
+}, 1000);
